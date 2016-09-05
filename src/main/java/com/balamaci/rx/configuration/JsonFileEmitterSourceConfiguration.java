8b8b078e1452dfa2950babb7cc0e2c31fb8a82dd
Serban Balamaci <serban.balamaci@1and1.ro>
2016-09-05 17:26:08 +0300

    start event emission on ContextRefreshEvent as to not lose any event

--- src/main/java/com/balamaci/rx/configuration/JsonFileEmitterSourceConfiguration.java
+++ src/main/java/com/balamaci/rx/configuration/JsonFileEmitterSourceConfiguration.java
@@ -6,9 +6,11 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import rx.Observable;
 import rx.Observer;
@@ -24,7 +26,7 @@ import static com.balamaci.rx.util.SleepUtil.sleepMillis;
  */
 @Configuration
 @Profile("hardcoded-events")
-public class JsonFileEmitterSourceConfiguration {
+public class JsonFileEmitterSourceConfiguration implements ApplicationListener<ContextRefreshedEvent> {
 
     private static final Logger log = LoggerFactory.getLogger(JsonFileEmitterSourceConfiguration.class);
 
@@ -36,9 +38,13 @@ public class JsonFileEmitterSourceConfiguration {
 
     @Bean(name = "events")
     public Observable<JsonObject> emitEvents(Receiver receiver) {
-        startEmitting(receiver);
-
         return receiver.getPublishSubject();
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        Receiver receiver = event.getApplicationContext().getBean(Receiver.class);
+        startEmitting(receiver);
     }
 
     private void startEmitting(Receiver receiver) {
